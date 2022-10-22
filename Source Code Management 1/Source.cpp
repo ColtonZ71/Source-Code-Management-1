73e561c13607c9761ec44fbd88ed223bfad189b8
Colton <Colton@DESKTOP-C34DK8I>
2022-10-21 22:05:52 -0400

    Add project files.

---
+++ Source Code Management 1/Source.cpp
@@ -0,0 +1,13 @@
+// Colton Batner
+// CIS 1202.501
+// 10-21-2022
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+
+	cout << endl << endl;
+	system("pause");
+	return 0;
+}
